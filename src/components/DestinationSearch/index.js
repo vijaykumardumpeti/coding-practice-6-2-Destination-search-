40c0331984123edc31c73204f46c4ae6ec6349b5
VIJAYKUMAR <115571763+vijaykumardumpeti@users.noreply.github.com>
2022-11-14 13:37:46 +0530

    Update index.js

--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -3,7 +3,7 @@
 import {Component} from 'react'
 import './index.css'
 
-import DestinationItem from './components/DestinationItem'
+import DestinationItem from '../DestinationItem'
 
 class DestinationSearch extends Component {
   state = {
@@ -17,26 +17,35 @@ class DestinationSearch extends Component {
   render() {
     const {searchInput} = this.state
 
+    const text = searchInput.toLowerCase()
+
     const {destinationsList} = this.props
 
     const searchResults = destinationsList.filter(eachObject =>
-      eachObject.name.includes(searchInput),
+      eachObject.name.toLowerCase().includes(text),
     )
 
     return (
-      <div>
-        <h1>Destination Search</h1>
-        <input
-          onChange={this.searchFunction}
-          placeholder="Search"
-          value={searchInput}
-        />
+      <div className="container">
+        <div>
+          <h1>Destination Search</h1>
+          <input
+            onChange={this.searchFunction}
+            placeholder="Search"
+            value={searchInput}
+            type="search"
+          />
+          <img
+            alt="search icon"
+            src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
+          />
+        </div>
 
-        <div className="images-container">
+        <ul className="images-container">
           {searchResults.map(eachObject => (
             <DestinationItem key={eachObject.id} eachObject={eachObject} />
           ))}
-        </div>
+        </ul>
       </div>
     )
   }
